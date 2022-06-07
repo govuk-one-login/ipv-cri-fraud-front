aef240d5ca8143a544cc35cfc53fc55e4bcd8114
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2022-06-07 11:17:26 +0100

    KBV-445: TechQA comments addressed. Updated stubs to align with backend changes

--- src/app/fraud/controllers/check.js
+++ src/app/fraud/controllers/check.js
@@ -8,15 +8,13 @@ const {
 
 class FraudCheckController extends BaseController {
   async saveValues(req, res, callback) {
-    const fraudCheck = await req.axios.post(`${CHECK}`, {}, {
+    await req.axios.post(`${CHECK}`, {}, {
       headers: {
         'Content-Type': 'application/application-json',
         'session_id': req.session.tokenId
       }
     });
 
-    req.session.authParams.authorization_code =
-      fraudCheck.data?.authorization_code;
 
     return super.saveValues(req, res, callback);
   }
