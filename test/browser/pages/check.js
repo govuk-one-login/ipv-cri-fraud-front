0d3acf94131eb03bd985725005f21586f444e95c
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2022-06-08 11:22:34 +0100

    refactor: Update ports to 5030 / 8030

--- test/browser/pages/check.js
+++ test/browser/pages/check.js
@@ -4,7 +4,7 @@ module.exports = class PlaywrightDevPage {
    */
   constructor(page) {
     this.page = page;
-    this.url = "http://localhost:5040/check";
+    this.url = "http://localhost:5030/check";
   }
 
   isCurrentPage() {
